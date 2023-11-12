de951a7c8de9eda989138626a66aaa7ce699fbba
MiltoC <cristian057355359@gmail.com>
2023-11-11 19:15:14 -0600

    Diseño de actualizar imagen de perfil y cuadro de dialogo

---
+++ app/src/main/java/com/example/agendasmart/views/ActualizarImagenPerfilActivity.java
@@ -0,0 +1,16 @@
+package com.example.agendasmart.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agendasmart.R;
+
+public class ActualizarImagenPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_imagen_perfil);
+    }
+}
