e89b8ed4978794491b9c4a53959a198c088bc060
marvin <lozanomarvin96@gmail.com>
2023-11-14 01:22:48 -0600

    Funcionalidad Eliminar y Actualizar

---
+++ app/src/main/java/com/example/agendasmart/views/ActualizarTareaActivity.java
@@ -0,0 +1,255 @@
+package com.example.agendasmart.views;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.agendasmart.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+
+import es.dmoral.toasty.Toasty;
+
+public class ActualizarTareaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    TextView Id_tarea_A, Uid_Usuario_A, Correo_usuario_A, Fecha_registro_A , Fecha_A, Estado_A, Estado_nuevo, fecha;
+    EditText Titulo_A, Descripcion_A;
+    Button Btn_Calendario_A;
+    ImageButton btnBack, btn_actualizar_tarea;
+
+    //Aqui se guarda los datos del registro seleccionado
+    String id_tarea_R , uid_usuario_R , correo_usuario_R, fecha_registro_R, titulo_R, descripcion_R, fecha_R, estado_R;
+
+    ImageView Tarea_Finalizada, Tarea_No_Finalizada;
+
+    Spinner Spinner_estado;
+
+    int dia, mes , anio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_nota);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Actualizar tarea");
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        InicializarVistas();
+        RecuperarDatos();
+        SetearDatos();
+        ComprobarEstadoNota();
+        Spinner_Estado();
+
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActualizarTareaActivity.super.onBackPressed();
+            }
+        });
+
+        btn_actualizar_tarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActualizarNotaBD();
+            }
+        });
+
+
+        Btn_Calendario_A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SeleccionarFecha();
+            }
+        });
+    }
+    private void InicializarVistas(){
+        Id_tarea_A = findViewById(R.id.itenid_A);
+        Correo_usuario_A = findViewById(R.id.correo_usuario_A);
+        Fecha_registro_A = findViewById(R.id.fecha_creacion_A);
+        Fecha_A = findViewById(R.id.fecha_hora_actual_A);
+        fecha = findViewById(R.id.fecha);
+        Estado_A = findViewById(R.id.estado_A);
+        Titulo_A = findViewById(R.id.titulo_A);
+        Descripcion_A = findViewById(R.id.descripcion_A);
+        Btn_Calendario_A = findViewById(R.id.btn_calendario_A);
+
+        Tarea_Finalizada = findViewById(R.id.Tarea_Finalizada);
+        Tarea_No_Finalizada = findViewById(R.id.Tarea_No_Finalizada);
+
+        Spinner_estado = findViewById(R.id.Spinner_estado);
+        Estado_nuevo = findViewById(R.id.Estado_nuevo);
+        btnBack = (ImageButton)findViewById(R.id.btnBack);
+        btn_actualizar_tarea = (ImageButton) findViewById(R.id.btn_actualizar_tarea);
+    }
+
+    private void RecuperarDatos(){
+        Bundle intent = getIntent().getExtras();
+
+        id_tarea_R = intent.getString("id_tarea");
+        //uid_usuario_R = intent.getString("uid_usuario");
+        correo_usuario_R = intent.getString("correo_usuario");
+        titulo_R = intent.getString("titulo");
+        descripcion_R = intent.getString("descripcion");
+        fecha_R = intent.getString("fecha_nota");
+        estado_R = intent.getString("estado");
+    }
+
+    private void SetearDatos(){
+        Id_tarea_A.setText(id_tarea_R);
+        //Uid_Usuario_A.setText(uid_usuario_R);
+        Correo_usuario_A.setText(correo_usuario_R);
+        Titulo_A.setText(titulo_R);
+        Descripcion_A.setText(descripcion_R);
+        fecha.setText(fecha_R);
+        Estado_A.setText(estado_R);
+
+    }
+
+    private void ComprobarEstadoNota(){
+        String estado_tarea = Estado_A.getText().toString();
+
+        if (estado_tarea.equals("No finalizada")){
+            Tarea_No_Finalizada.setVisibility(View.VISIBLE);
+        }
+        if (estado_tarea.equals("Finalizada")){
+            Tarea_Finalizada.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void SeleccionarFecha(){
+        final Calendar calendario = Calendar.getInstance();
+        dia = calendario.get(Calendar.DAY_OF_MONTH);
+        mes = calendario.get(Calendar.MONTH);
+        anio = calendario.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(ActualizarTareaActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int anioSeleccionado, int mesSeleccionado, int diaSeleccionado) {
+
+                // Obtén la fecha actual
+                Calendar fechaActual = Calendar.getInstance();
+
+                // Crear una instancia de la fecha seleccionada
+                Calendar fechaSeleccionada = Calendar.getInstance();
+                fechaSeleccionada.set(anioSeleccionado, mesSeleccionado, diaSeleccionado);
+
+                // Verificar si la fecha seleccionada es anterior a la actual
+                if (fechaSeleccionada.before(fechaActual)) {
+                    // Muestra un mensaje o realiza la acción que desees cuando la fecha es anterior
+                    Toasty.error(ActualizarTareaActivity.this, "No se pueden seleccionar fechas anteriores", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Formatea y muestra la fecha
+                    String diaFormateado, mesFormateado;
+                    if (diaSeleccionado < 10) {
+                        diaFormateado = "0" + String.valueOf(diaSeleccionado);
+                    } else {
+                        diaFormateado = String.valueOf(diaSeleccionado);
+                    }
+
+                    int Mes = mesSeleccionado + 1;
+
+                    if (Mes < 10) {
+                        mesFormateado = "0" + String.valueOf(Mes);
+                    } else {
+                        mesFormateado = String.valueOf(Mes);
+                    }
+
+                    /*Mostrar fecha*/
+                    fecha.setText(diaFormateado + "/" + mesFormateado + "/" + anioSeleccionado);
+                }
+            }
+        },anio, mes, dia);
+        datePickerDialog.show();
+
+    }
+
+    private void Spinner_Estado(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Estados_tarea, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner_estado.setAdapter(adapter);
+        Spinner_estado.setOnItemSelectedListener(this);
+
+    }
+
+    private void ActualizarNotaBD(){
+        String tituloActualizar = Titulo_A.getText().toString();
+        String descripcionActualizar = Descripcion_A.getText().toString();
+        String fechaActualizar = fecha.getText().toString();
+        String estadoActualizar = Estado_nuevo.getText().toString();
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("Tareas_publicadas");
+
+        //Consulta
+        Query query = databaseReference.orderByChild("id_tarea").equalTo(id_tarea_R);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ds.getRef().child("titulo").setValue(tituloActualizar);
+                    ds.getRef().child("descripcion").setValue(descripcionActualizar);
+                    ds.getRef().child("fecha_nota").setValue(fechaActualizar);
+                    ds.getRef().child("estado").setValue(estadoActualizar);
+                }
+
+                Toast.makeText(ActualizarTareaActivity.this, "Tarea actualizada con éxito", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String ESTADO_ACTUAL = Estado_A.getText().toString();
+
+        String Posicion_1 = adapterView.getItemAtPosition(1).toString();
+
+        String estado_seleccionado = adapterView.getItemAtPosition(i).toString();
+        Estado_nuevo.setText(estado_seleccionado);
+
+        if (ESTADO_ACTUAL.equals("Finalizada")){
+            Estado_nuevo.setText(Posicion_1);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+}
--- app/src/main/java/com/example/agendasmart/views/MisTareasActivity.java
+++ app/src/main/java/com/example/agendasmart/views/MisTareasActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.agendasmart.Objetos.Tarea;
 import com.example.agendasmart.R;
@@ -18,16 +24,20 @@ import com.example.agendasmart.ViewHolder.ViewHolder_Tarea;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
 
 import es.dmoral.toasty.Toasty;
 
 public class MisTareasActivity extends AppCompatActivity {
 
-    ImageButton btnBack;
+    ImageButton btnBack, btnEliminar;
     RecyclerView recyclerViewTareas;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference Base_De_Datos;
@@ -39,6 +49,9 @@ public class MisTareasActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
     FirebaseUser user;
+
+    Dialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +73,10 @@ public class MisTareasActivity extends AppCompatActivity {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         Base_De_Datos = firebaseDatabase.getReference("Tareas_publicadas");
-
+        dialog = new Dialog(MisTareasActivity.this);
         ListarTareasUsuarios();
 
-
+        btnEliminar = (ImageButton) findViewById(R.id.btnEliminar);
         btnBack = (ImageButton) findViewById(R.id.btnBack);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +87,7 @@ public class MisTareasActivity extends AppCompatActivity {
             }
         });
     }
+
 
     private void ListarTareasUsuarios(){
 
@@ -103,22 +117,112 @@ public class MisTareasActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(View view, int position) {
                         Toasty.info(MisTareasActivity.this, "on item click", Toasty.LENGTH_SHORT).show();
+
                     }
 
                     @Override
                     public void onItemLongClick(View view, int position) {
-                        Toasty.info(MisTareasActivity.this, "on item Long click", Toasty.LENGTH_SHORT).show();
+
+                        //Obtener los datos de la nota seleccionada
+                        String id_tarea = getItem(position).getId_tarea();
+                       // String uid_usuario = getItem(position).getUid_usuario();
+                        String correo_usuario = getItem(position).getCorreo_usuario();
+                        String fecha_registro = getItem(position).getFecha_horaactual();
+                        String titulo = getItem(position).getTitulo();
+                        String descripcion = getItem(position).getDescripcion();
+                        String fecha_nota = getItem(position).getFecha_nota();
+                        String estado = getItem(position).getEstado();
+
+
+                        Button Eliminar, Actualizar;
+                        ImageButton btnEliminar;
+
+
+                        //Realizar la conexión con el diseño
+                        dialog.setContentView(R.layout.cuadro_dialogo_opciones);
+
+                        //Inicializar las vistas
+                        Eliminar = dialog.findViewById(R.id.Eliminar);
+                        Actualizar = dialog.findViewById(R.id.Actualizar);
+
+
+                        Eliminar.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                EliminarNota(id_tarea);
+                                dialog.dismiss();
+                            }
+                        });
+
+                        Actualizar.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                //Toast.makeText(MisTareasActivity.this, "Actualizar Tarea", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MisTareasActivity.this, ActualizarTareaActivity.class);
+                                intent.putExtra("id_tarea", id_tarea);
+                                //intent.putExtra("uid_usuario", uid_usuario);
+                                intent.putExtra("correo_usuario", correo_usuario);
+                                intent.putExtra("fecha_registro", fecha_registro);
+                                intent.putExtra("titulo", titulo);
+                                intent.putExtra("descripcion", descripcion);
+                                intent.putExtra("fecha_nota", fecha_nota);
+                                intent.putExtra("estado", estado);
+                                startActivity(intent);
+                                dialog.dismiss();
+                            }
+                        });
+                        dialog.show();
                     }
                 });
                 return viewHolder_tarea;
             }
         };
+
+
+
         linearLayoutManager= new LinearLayoutManager(MisTareasActivity.this, LinearLayoutManager.VERTICAL, false);
         linearLayoutManager.setReverseLayout(true);/*Listar notas desde la ultima*/
         linearLayoutManager.setStackFromEnd(true);/*empiece desde la parte superior*/
 
         recyclerViewTareas.setLayoutManager(linearLayoutManager);
         recyclerViewTareas.setAdapter(firebaseRecyclerAdapter);
+    }
+
+
+    private void EliminarNota(String id_tarea) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MisTareasActivity.this);
+        builder.setTitle("Eliminar Tarea");
+        builder.setMessage("¿Desea Eliminar la Tarea?");
+        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //ELIMINAR NOTA EN BD
+                Query query = Base_De_Datos.orderByChild("id_tarea").equalTo(id_tarea);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                        for (DataSnapshot ds : snapshot.getChildren()){
+                            ds.getRef().removeValue();
+                        }
+                        Toast.makeText(MisTareasActivity.this, "Tarea eliminada", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                        Toast.makeText(MisTareasActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MisTareasActivity.this, "Cancelado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.create().show();
     }
 
     @Override
